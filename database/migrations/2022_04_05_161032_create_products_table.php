1fb274bd3dc6391c4d3304b208aadf46ea975499
shahariarikbal <shahariar.ikbal86@gmail.com>
2022-04-05 22:47:59 +0600

    feat:Model and migration

---
+++ database/migrations/2022_04_05_161032_create_products_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('cat_id');
+            $table->unsignedBigInteger('brand_id');
+            $table->string('name');
+            $table->float('price', 8, 2);
+            $table->float('discount_price', 8, 2)->nullable();
+            $table->unsignedBigInteger('stock');
+            $table->unsignedBigInteger('qty');
+            $table->string('sku');
+            $table->text('short_description');
+            $table->longText('long_description');
+            $table->longText('information');
+            $table->string('type');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
