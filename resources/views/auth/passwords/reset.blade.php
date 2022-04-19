a2df48b6853beef0956853bc22fa29a85fe9c981
Ahmed Sabbir <theahmedsabbir@gmail.com>
2022-04-19 21:55:06 +0600

    home page quick login done

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -8,9 +8,10 @@
                 <div class="card-header">{{ __('Reset Password') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="">
+                    <form method="POST" action="{{ route('password.update') }}">
                         @csrf
 
+                        <input type="hidden" name="token" value="{{ $token }}">
 
                         <div class="row mb-3">
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
--- resources/views/frontend/includes/header.blade.php
+++ resources/views/frontend/includes/header.blade.php
@@ -224,18 +224,29 @@
                                         <li class="active">
                                             <a data-toggle="tab" aria-expanded="true" href="#header-tab-login">Login</a>
                                         </li>
-                                        <li>
+{{--                                         <li>
                                             <a data-toggle="tab" aria-expanded="true" href="#header-tab-rigister">Register</a>
-                                        </li>
+                                        </li> --}}
                                     </ul>
                                     <div class="tab-container">
                                         <div id="header-tab-login" class="tab-panel active">
-                                            <form method="post" class="login form-login">
+                                            <form method="POST" action="{{ url('/login') }}" class="login form-login">
+                                                @csrf
+                                                
                                                 <p class="form-row form-row-wide">
-                                                    <input type="email" placeholder="Email" class="input-text">
+                                                    <input type="email" type="email" name="email"
+                                                        required placeholder="Email" class="input-text">
+
+                                                    @if ($errors->has('email'))
+                                                        <p class="text-danger" style="font-size: 14px;">{{ $errors->first('email') }}</p>
+                                                    @endif
                                                 </p>
                                                 <p class="form-row form-row-wide">
-                                                    <input type="password" class="input-text" placeholder="Password">
+                                                    <input type="password" name="password" class="input-text" placeholder="Password" required>
+
+                                                    @if ($errors->has('password'))
+                                                        <p class="text-danger" style="font-size: 14px;">{{ $errors->first('password') }}</p>
+                                                    @endif
                                                 </p>
                                                 <p class="form-row">
                                                     <label class="form-checkbox">
@@ -247,7 +258,10 @@
                                                     <input type="submit" class="button" value="Login">
                                                 </p>
                                                 <p class="lost_password">
-                                                    <a href="#">Lost your password?</a>
+                                                    <a href="{{ url('password/reset') }}">Lost your password?</a>
+                                                </p>
+                                                <p class="lost_password">
+                                                    <a href="{{ url('register') }}">Register here</a>
                                                 </p>
                                             </form>
                                         </div>
