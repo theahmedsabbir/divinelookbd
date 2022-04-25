d5004d3e0ff1d782a057a4eeb830e4712be2592e
Ahmed Sabbir <theahmedsabbir@gmail.com>
2022-04-26 03:33:57 +0600

    udpate

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('migrate', function() {
+    \Artisan::call('migrate');
+    return "migrated!";
+});
+
 Route::get('cache', function() {
     \Artisan::call('cache:clear');
     \Artisan::call('config:clear');
