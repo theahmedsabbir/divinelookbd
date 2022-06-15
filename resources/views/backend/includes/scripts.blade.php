8da33ecf881397a9d4979b4fbd8376cb7efc71ca
Ahmed Sabbir <theahmedsabbir@gmail.com>
2022-06-15 16:15:34 +0600

    excel fix

--- resources/views/backend/includes/scripts.blade.php
+++ resources/views/backend/includes/scripts.blade.php
@@ -9,12 +9,12 @@
     {{-- <script src="{{ asset('backend/lib/datatables.net-responsive-dt/js/responsive.dataTables.min.js') }}"></script> --}}
     {{-- <script src="{{ asset('backend/js/jquery.dataTables.min.js') }}"></script> --}}
 
-    <script src="http://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
-  <script src="https://cdn.datatables.net/buttons/2.1.0/js/dataTables.buttons.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-  <script src="https://cdn.datatables.net/buttons/2.1.0/js/buttons.html5.min.js"></script>
+  <script src="{{ asset('backend/js/jquery.dataTables.min.js') }}"></script>
+  <script src="{{ asset('backend/js/dataTables.buttons.min.js') }}"></script>
+  <script src="{{ asset('backend/js/jszip.min.js') }}"></script>
+  <script src="{{ asset('backend/js/pdfmake.min.js') }}"></script>
+  <script src="{{ asset('backend/js/vfs_fonts.js') }}"></script>
+  <script src="{{ asset('backend/js/buttons.html5.min.js') }}"></script>
 
     <script src="{{ asset('backend/lib/summernote/summernote-bs4.min.js') }}"></script>
     <script src="{{ asset('backend/lib/medium-editor/js/medium-editor.min.js') }}"></script>
